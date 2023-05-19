3d0edbcdae613c94a5393df8d1628be9cf29b33a
xiexuefeng <1158231926@qq.com>
2023-05-19 17:42:43 +0800

    wip(lab1): part2

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
@@ -67,7 +68,7 @@ def part1_calculate_T_pose(bvh_file_path):
     return joint_name, joint_parent, np.array(joint_offset)
 
 
-def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data, frame_id):
+def part2_forward_kinematics(joint_name: list[str], joint_parent: list[int], joint_offset: np.ndarray, motion_data: np.ndarray, frame_id: int):
     """请填写以下内容
     输入: part1 获得的关节名字，父节点列表，偏移量列表
         motion_data: np.ndarray，形状为(N,X)的numpy数组，其中N为帧数，X为Channel数
@@ -79,9 +80,39 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
         1. joint_orientations的四元数顺序为(x, y, z, w)
         2. from_euler时注意使用大写的XYZ
     """
-    joint_positions = None
-    joint_orientations = None
-    return joint_positions, joint_orientations
+    joint_positions = []
+    joint_orientations = []
+    frame_motion: np.ndarray = motion_data[frame_id]
+    root_pos = frame_motion[0: 3]
+    root_orientation = R.from_euler('XYZ', frame_motion[3: 6], degrees=True).as_quat()
+
+    joint_positions.append(root_pos)
+    joint_orientations.append(root_orientation)
+    channel_index = 0
+
+    # print('motion: ', type(frame_motion[0: 3]))
+
+    for i in range(1, len(joint_name)):
+        is_end = joint_name[i].endswith('_end')
+        # 由于末端节点没有channel信息，所以不能简单的用当前数组索引进行取值
+        if not is_end:
+            channel_index += 1
+
+        parent_index = joint_parent[i]
+        parent_orientation = R.from_quat(joint_orientations[parent_index])
+        parent_pos: np.ndarray = joint_positions[parent_index]
+        cur_offset: np.ndarray = joint_offset[i]
+        start_index = 6 + (channel_index - 1) * 3
+        # print(frame_motion[start_index: start_index + 3])
+        cur_rotation = R.from_euler('XYZ', frame_motion[start_index: start_index + 3], degrees=True)
+        # 当前关节的全局坐标位置
+        cur_pos = parent_pos + parent_orientation.apply(cur_offset)
+        # 当前关节的全局朝向（旋转）；末端节点没有自身的rotation信息，只有相对位移
+        cur_orientation = parent_orientation if is_end else parent_orientation * cur_rotation
+        joint_positions.append(cur_pos)
+        joint_orientations.append(cur_orientation.as_quat())
+
+    return np.array(joint_positions), np.array(joint_orientations)
 
 
 def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
--- lab1/task1_forward_kinematics.py
+++ lab1/task1_forward_kinematics.py
@@ -71,18 +71,18 @@ def part3_retarget(viewer, T_pose_bvh_path, A_pose_bvh_path):
 def main():
     # create a viewer
     viewer = SimpleViewer()
-    bvh_file_path = "data/walk60.bvh"
+    bvh_file_path = "data/0017_SpeedVault001.bvh"
 
     # 请取消注释需要运行的代码
     # part1
-    part1(viewer, bvh_file_path)
+    # part1(viewer, bvh_file_path)
 
     # part2
     # part2_one_pose(viewer, bvh_file_path)
-    # part2_animation(viewer, bvh_file_path)
+    part2_animation(viewer, bvh_file_path)
 
     # part3
-    part3_retarget(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
+    # part3_retarget(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
 
 
 if __name__ == "__main__":
